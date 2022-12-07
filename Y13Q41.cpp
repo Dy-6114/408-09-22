5143963d3a8395fe66dbd0faba92683e5bc432c4
ivandong <dongivan6114@gmail.com>
2022-12-07 17:26:17 +0800

    13年41题

--- Y13Q41.cpp
+++ Y13Q41.cpp
@@ -39,14 +39,50 @@ int GetMajority(int A[], int n)
         return max;
     return -1;
 }
-/**/
+/*更优解：
+1.设计思想：策略是从前向后扫描数组元素，标记出一个可能成为主元素的元素Num。然后重新计数，确认num是否是主元素
+（1）选取候选的主元素：依次扫描所给数组中的每个整数，将第一个遇到的整数Num保存到c中，记录Num的出现次数为1；
+若遇到的下一个整数仍等于Num，则计数+1，否则计数-1；当计数减到0时，将遇到的下一个整数保存到c中，计数重新记为1，开始新一轮计数，
+即从当前位置开始重复上述过程，直到扫描完全部数组元素。
+（2）判断c元素是否是真正的主元素：再次扫描该数组，统计c中元素出现的次数，若大于n/2，则为主元素；否则序列中不存在主元素。
+2.算法实现如下：
+3.时间复杂度：O(n)
+空间复杂度:O(1)
+*/
+
+int GetMajority_pro(int A[],int n)
+{
+    int c, count = 1;
+    c = A[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (A[i] == c) count++;
+        else if(count>0) count--;
+        else{
+            count = 1;
+            c = A[i];
+        }
+    }
+    if(count>0)
+        for (int i = count = 0; i < n; i++)
+        {
+            if (A[i] == c) count++;
+        }
+    if(count > n/2) return c;
+    return -1;   
+    
+}
 
 
 
 int main(){
     int B[] = {0,5,5,3,5,1,5,7};
     int A[] = {0,5,5,3,5,7,5,5};
+    cout<<"一般解法"<<endl;
     cout<<"A序列中的主元素为:"<<GetMajority(A,8)<<endl;
-    cout<<"B序列中的主元素为:"<<GetMajority(B,8);
+    cout<<"B序列中的主元素为:"<<GetMajority(B,8)<<endl;
+    cout<<"更优解法"<<endl;
+    cout<<"A序列中的主元素为:"<<GetMajority_pro(A,8)<<endl;
+    cout<<"B序列中的主元素为:"<<GetMajority_pro(B,8);
     return 0;
 }
